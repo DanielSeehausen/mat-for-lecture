e8427308a11955ddf1439008bcb13e3b0fa8d756
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-12-01 16:59:38 -0500

    changing for monday

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,43 +1,37 @@
 // CONFIG***********************************************************************
-const COLCOUNT = 10
-const ROWCOUNT = 10
 const WSSERVER = 'ws://192.168.4.70:8080'
-var PLAYERVAL = 'U'
+
+// WSCONFIG*********************************************************************
+var COLCOUNT = null
+var ROWCOUNT = null
+var PLAYERID = null
 var PLAYERCOLOR = null
 var CURRPOS = null
-var VALMAT = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+var VALMAT = null
+var DOMMAT = null
+var PLAYERCOLORS = null
 
 // TITLE-INIT*******************************************************************
-function fadeIn(el) {
-  el.classList.add("bounce")
-  console.log('wot');
+function titleInit() {
+  function fadeIn(el) {
+    el.classList.add("bounce")
+    console.log('wot');
+  }
+
+  function delayedFadeIn(div, range, min=0) {
+    setTimeout(() => {
+      fadeIn(div)
+    }, Math.random() * range + min)
+  }
+
+  function fadeLettersIn() {
+    Array.from(document.getElementsByClassName("title-letter")).forEach(div => {
+      delayedFadeIn(div, 10000)
+    })
+  }
+  fadeLettersIn()
 }
-
-function delayedFadeIn(div, range, min=0) {
-  setTimeout(() => {
-    fadeIn(div)
-  }, Math.random() * range + min)
-}
-
-function fadeLettersIn() {
-  Array.from(document.getElementsByClassName("title-letter")).forEach(div => {
-    delayedFadeIn(div, 10000)
-  })
-}
-
-fadeLettersIn()
-
+titleInit()
 
 // INIT*************************************************************************
 function genMatrixHTML() {
@@ -61,15 +55,14 @@ function getMatrixRefs() {
   return domArr.map(row => (Array.from(row.childNodes)))
 }
 
-const matrixHTML = genMatrixHTML()
-appendMatrix(matrixHTML)
-const DOMMATRIX = getMatrixRefs()
 // MATFUNCS*********************************************************************
-
 function mapValsToDom(valMat, domMat) {
   for (let rIdx = 0; rIdx < ROWCOUNT; rIdx++) {
-    for (let cIdx = 0; cIdx < COLCOUNT; cIdx++)
+    for (let cIdx = 0; cIdx < COLCOUNT; cIdx++) {
       domMat[rIdx][cIdx].innerHTML = valMat[rIdx][cIdx]
+      if (valMat[rIdx][cIdx] === PLAYERID)
+        domMat[rIdx][cIdx].style.backgroundColor = PLAYERCOLOR
+    }
   }
 }
 
@@ -79,21 +72,17 @@ function outOfBounds(newLoc) {
 }
 
 function isValidMove(valMat, currPos, jaunt) {
-  console.log(CURRPOS, jaunt);
   const newLoc = [currPos[0] + jaunt[0], currPos[1] + jaunt[1]]
-  return (!outOfBounds(newLoc) && valMat[newLoc[0]][newLoc[1]] === 0)
+  return (!outOfBounds(newLoc) && (valMat[newLoc[0]][newLoc[1]] === 0 || valMat[newLoc[0]][newLoc[1]] === 3))
 }
 
-const DIRECTIONS = [[0, -1], [1, 0], [0, 1], [-1, 0]]
 function getAvailableDirections(valMat, currPos) {
-  return DIRECTIONS.filter(jaunt => isValidMove(valMat, currPos, jaunt))
+  return [[-1, 0], [0, 1], [1, 0], [0, -1]].filter(jaunt => isValidMove(valMat, currPos, jaunt))
 }
 
 function move(oldPos, jaunt) {
   return [oldPos[0] + jaunt[0], oldPos[1] + jaunt[1]]
 }
-
-mapValsToDom(VALMAT, DOMMATRIX)
 
 // WS***************************************************************************
 const GREETING = "Client websocket opened"
@@ -112,20 +101,29 @@ ws.addEventListener('message', (event) => {
   const msg = JSON.parse(event.data)
   console.log(msg)
   if (msg.action === 'init') {
-    CURRPOS = msg.data.pos
-    PLAYERVAL = msg.data.val
+    CURRPOS = msg.data.startPos
+    PLAYERID = msg.data.id
     PLAYERCOLOR = msg.data.color
-    DOMMATRIX[CURRPOS[0]][CURRPOS[1]].innerHTML = PLAYERVAL
-    DOMMATRIX[CURRPOS[0]][CURRPOS[1]].style.backgroundColor = PLAYERCOLOR
+    VALMAT = msg.data.mat
+    COLCOUNT = msg.data.rowCount
+    ROWCOUNT = msg.data.colCount
+    PLAYERCOLORS = msg.data.playerColors
+    appendMatrix(genMatrixHTML(ROWCOUNT, COLCOUNT))
+    DOMMAT = getMatrixRefs()
+    debugger
+    mapValsToDom(VALMAT, DOMMAT)
   }
+
   if (msg.action === 'playerPos') {
     CURRPOS = msg.data
-    DOMMATRIX[CURRPOS[0]][CURRPOS[1]].style.backgroundColor = PLAYERCOLOR
+    DOMMAT[CURRPOS[0]][CURRPOS[1]].style.backgroundColor = PLAYERCOLOR
   }
+
   if (msg.action === 'matrix') {
     VALMAT = msg.data
-    mapValsToDom(VALMAT, DOMMATRIX)
+    mapValsToDom(VALMAT, DOMMAT)
   }
+
 })
 
 // Connection closed
--- wsServer/index.js
+++ wsServer/index.js
@@ -1,41 +1,44 @@
 var WebSocketServer = require('ws').Server
 var wss = new WebSocketServer({port: 8080})
 
-const MATRIX = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+const ROWCOUNT = 15
+const COLCOUNT = 15
+const DEFAULTMATRIX = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
+const DEFAULTPLAYERSDATA = [
+  [1,  'blue',   [0, 0]],
+  [2,  'red',    [0, 3]],
+  [3,  'yellow', [0, 6]],
+  [4,  'grey',   [0, 9]],
+  [5,  'purple', [4, 0]],
+  [6,  'cyan',   [4, 3]],
+  [7,  'orange', [4, 6]],
+  [8,  'pink',   [4, 9]],
+  [9,  'black',  [9, 0]],
+  [10, 'green',  [9, 3]],
+  [11, 'violet', [9, 6]],
+  [12, 'white',  [9, 9]]
+]
+const PLAYERIDSTOCOLOR = DEFAULTPLAYERSDATA.map(pArr => ({id: pArr[1]}))
 
-const startPositions = [
-  [0, 3],
-  [0, 6],
-  [0, 9],
-  [2, 3],
-  [2, 6],
-  [2, 9],
-  [4, 3],
-  [4, 6],
-  [4, 9],
-  [6, 3],
-  [6, 6],
-  [6, 9],
-  [8, 3],
-  [8, 6],
-  [8, 9]
-]
-const playerVals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].reverse()
-const playerColors = ['blue', 'red', 'yellow', 'grey', 'purple', 'cyan', 'orange', 'pink', 'black', 'green', 'violet'].reverse()
-// const gameMat = MATRIX.map(row => row.slice())
+const playerStartData = DEFAULTPLAYERSDATA.slice().reverse()
+const gameMat = DEFAULTMATRIX.map(row => row.slice())
 const members = new Set()
-
 function emit(type, data) {
   members.forEach(ws => {
     ws.send(JSON.stringify({action: type, data: data}))
@@ -44,30 +47,33 @@ function emit(type, data) {
 
 wss.on('connection', (ws, req) => {
     const ip = req.connection.remoteAddress
-    console.log(ip, 'connected');
-
+    console.log(ip, 'attempting to connect...');
+    if (playerStartData.length === 0) {
+      ws.close()
+      return
+    }
+    console.log(ip, '...done!');
     members.add(ws)
-    let pos = startPositions.pop()
-    const val = playerVals.pop()
-    MATRIX[pos[0]][pos[1]] = val
-    ws.send(JSON.stringify({action: 'init', data: {pos: pos, val: val, color: playerColors.pop()}}))
-    emit('matrix', MATRIX)
+    var [id, color, currPos] = playerStartData.pop()
+    gameMat[currPos[0]][currPos[1]] = id
+    ws.send(JSON.stringify({action: 'init', data: {startPos: currPos, id: id, color: color, mat: gameMat, rowCount: ROWCOUNT, colCount: COLCOUNT, playerColors: PLAYERIDSTOCOLOR}}))
+    emit('matrix', gameMat)
 
     ws.on('message', (payload) => {
       const msg = JSON.parse(payload)
-      console.log("\nreceived", payload)
+      console.log("\n", ip, " received" , payload)
       if (msg.action === 'move') {
-        MATRIX[msg.data[0]][msg.data[1]] = val
-        MATRIX[pos[0]][pos[1]] = 0
-        pos = msg.data
-        emit('matrix', MATRIX)
+        gameMat[msg.data[0]][msg.data[1]] = id
+        gameMat[currPos[0]][currPos[1]] = 0
+        currPos = msg.data
+        emit('matrix', gameMat)
         ws.send(JSON.stringify({action: 'playerPos', data: [msg.data[0], msg.data[1]]}))
       }
     })
 
     ws.on('close', () => {
       members.delete(ws)
-      MATRIX[pos[0]][pos[1]] = 0
+      gameMat[currPos[0]][currPos[1]] = 0
       console.log(ip, 'disconnected');
     })
 
